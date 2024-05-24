9247c71a1da48ad0afa5c8d8f6b7eb904feff065
TaldoRichard <richarddegodois@gmail.com>
2024-05-24 17:08:39 -0300

    Solução parte 1 (ta com problema desconhecido)

--- src/aplicação/Programa.java
+++ src/aplicação/Programa.java
@@ -1,11 +1,33 @@
 package aplicação;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model_entities.CarRental;
+import model_entities.Vehicle;
+
 public class Programa {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 		
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		
+		System.out.println("Digite os dados do aluguel");
+		System.out.print("Modelo do carro: ");
+		String carModel = sc.nextLine();
+		System.out.print("Retirada (dd/MM/yyyy hh:mm): ");
+		LocalDateTime start = new LocalDateTime.parse(sc.nextLine(), fmt);
+		System.out.print("Retorno (dd/MM/yyyy hh:mm): ");
+		LocalDateTime finish = new LocalDateTime(sc.nextLine(), fmt);
+		
+		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
 
+		sc.close();
 	}
 
 }
